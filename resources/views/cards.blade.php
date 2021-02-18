302570adcf347d29222f27e981bbc88d61f582d4
unknown <jmdnbvsjm@gmail.com>
2021-02-17 23:22:03 -0400

    tarjetas finales

---
+++ resources/views/cards.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.app')
+
+@section('contenido')
+        <div class="container">
+                <div class="row my-2">
+                 <h2 class=" m-auto p-2 principal-title">Potencie y mejore sus comprar con nuestros servicios adicionales</h2>
+                </div>
+                
+                <div class="row col-12 m-auto   ">
+
+
+                        <div class="col-md-auto col-12 borde  mr-2  tarjeta-adicionales border shadow-sm"> 
+                                <div class="row px-3">
+                                <h3>Capacitaciones</h3>
+                                </div>
+
+                                <div class="row  px-3">
+                                        <span class="sub-title">Invierta en la capacitación de sus trabajadores</span>
+                                </div>
+
+                                <div class="plan-addon mx-3">
+                                        <div class="row caracteristica">
+                                                <p class="col-5 col-md-6  col-xl-6">Plataforma Seven Academy </p> 
+                                                <div class="feature-prince col">
+                                                        <span class="">10$/ Usuario adicional por mes</span>
+                                                </div>
+                                        </div>
+                                </div>
+                               
+
+                        </div>
+
+
+                        <div class= "col-md-auto col-12 tarjeta-adicionales  border shadow-sm"> 
+                               
+                                <div class="row  px-3">
+                                <h3>Implementación</h3>
+                                </div>
+
+                                <div class="row  px-3">
+                                        <span class="sub-title">Apóyese en nosotros para la implementación y configuración de los módulos adquiridos</span>
+                                </div>
+
+                                <div class="plan-addon mx-3">
+                                        <div class="row caracteristica">
+                                                <p class="col-11">Plan Stater </p> 
+                                                <div class="feature-prince  col-1 ">
+                                                        <span>125$</span>
+                                                </div>
+                                        </div>
+                                        <div class="row caracteristica">
+                                                <p class="col-11">Plan Premium </p> 
+                                                <div class="feature-prince  col-1  ">
+                                                        <span>250$</span>
+                                                </div>
+                                        </div>
+                                        <div class="row caracteristica">
+                                                <p class="col-11">Plan Premium </p> 
+                                                <div class="feature-prince  col-1">
+                                                        <span>500$</span>
+                                                </div>
+                                        </div>
+                                </div>
+                               
+
+                        </div>
+
+                     
+
+                </div>
+                <div class="row col-12 m-auto ">
+                        <div class="col-12 col-md-12 col-lg-12 tarjeta-adicionales border shadow-sm"> 
+                               
+                                <div class="row  px-3">
+                                <h3>Autorice a sus usuarios</h3>
+                                </div>
+
+                                <div class="row  px-3">
+                                        <span class="sub-title">Adquiera Seven Management para que maneje el acceso y permisos a sus usuarios en los distintos módulos</span>
+                                </div>
+
+                                <div class="plan-addon mx-3">
+                                        <div class="row caracteristica">
+                                                <p class="col-11">Seven Management</p> 
+                                                <div class="feature-prince  col-1 ">
+                                                        <span>10$</span>
+                                                </div>
+                                        </div>
+                                     
+                                </div>
+                        </div>
+                </div>
+               
+        </div>    
+
+@endsection
